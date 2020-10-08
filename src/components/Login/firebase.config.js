536212c1fdf9b17e9dcb1792ef4e3d0939fc89a7
ayshi roy <ayshiroy22@gmail.com>
2020-10-08 13:30:44 -0700

    finish project

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCnJM45q7YyE64fgVn1TQDuRXoABrDqgiM",
+  authDomain: "ema-john-with-auth-c7b12.firebaseapp.com",
+  databaseURL: "https://ema-john-with-auth-c7b12.firebaseio.com",
+  projectId: "ema-john-with-auth-c7b12",
+  storageBucket: "ema-john-with-auth-c7b12.appspot.com",
+  messagingSenderId: "254334927056",
+  appId: "1:254334927056:web:724df126f6d4f30c141ed5"
+  };
+  export default firebaseConfig;
